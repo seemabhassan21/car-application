6346254476733ba2c282e9c99b921b3128dbf26e
Seemab Hassan <seemab@wanclouds.net>
2025-08-26 23:49:58 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-set -e
+set -eu
 
 [ -f .env ] && export $(grep -v '^#' .env | xargs)
 
-until nc -z "$DB_HOST" "$DB_PORT"; do sleep 2; done
+until nc -z "${DB_HOST:?}" "${DB_PORT:?}"; do sleep 2; done
 
 alembic upgrade head
 exec uvicorn main:app --host 0.0.0.0 --port 8000
